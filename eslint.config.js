f46a830bb4915df33344f075cf2588b34b61475a
midudev <miduga@gmail.com>
2024-12-10 11:50:57 +0100

    feat: Add ESLint configuration for Astro and JSX, defining rules and file specifications for better code quality

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslintPluginAstro from 'eslint-plugin-astro'
+import neostandard from 'neostandard'
+
+export default [
+  ...neostandard(),
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: ['.astro/**/*.*'],
+    files: ['**/*.astro', '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    rules: {
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/jsx-indent': 'off',
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'react/self-closing-comp': 'off',
+      'react/jsx-key': 'off',
+      '@stylistic/jsx-closing-tag-location': 'off',
+      '@stylistic/jsx-closing-bracket-location': 'off',
+    },
+  },
+]

429b9fa49c3beebafbebd299329434ac685c9059
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-12-14 14:49:29 +0100

    feat: add all links and reference to them in their respective buttons. Created LinkEntrada component based on Buttom and a file for consts

---
+++ src/consts/links.ts
@@ -0,0 +1,3 @@
+export const LUGAR_MAPA = 'https://maps.app.goo.gl/AWd5ohsAqWvzc7En7';
+export const COMPRAR_ENTRADA = 'https://ti.to/js-conf/js-conf-2025/with/earlybird';
+export const QUIERO_PATROCINAR = 'mailto:miduga@gmail.com'

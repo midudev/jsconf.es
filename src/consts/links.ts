0e96772604a613e517bf68faebf14ad2fbc4ed48
midudev <miduga@gmail.com>
2024-12-19 13:00:05 +0100

    fix: update discount code in LinkEntrada and COMPRAR_ENTRADA to reflect holiday promotion

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,3 +1,4 @@
 export const LUGAR_MAPA = 'https://maps.app.goo.gl/AWd5ohsAqWvzc7En7'
-export const COMPRAR_ENTRADA = 'https://ti.to/js-conf/js-conf-2025/with/earlybird'
+export const COMPRAR_ENTRADA =
+  'https://ti.to/js-conf/js-conf-2025/with/earlybird?discount_code=XMAS'
 export const QUIERO_PATROCINAR = 'mailto:miduga@gmail.com'

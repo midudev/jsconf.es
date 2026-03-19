3fc4d6ba97bc07a5a1bcc76b1bffa0df8c5340b8
madeval <alvarez.fing@gmail.com>
2026-03-19 17:25:12 -0300

    feat: add React integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jsconf.es',
   output: 'server',
-  integrations: [sitemap({ changefreq: 'weekly' })],
+  integrations: [sitemap({ changefreq: 'weekly' }), react()],
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
